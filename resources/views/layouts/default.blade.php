2411e5c220bf49942e5cd73c919f3c86d04ff352
liujuntao <ljt1018168@163.com>
2017-12-27 09:22:03 +0000

    finish user sign

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,23 +5,14 @@
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    <header class="navbar navbar-fixed-top navbar-inverse">
-      <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-          <a href="/" id="logo">Sample App</a>
-          <nav>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="{{route('help')}}">帮助</a></li>
-              <li><a href="#">登录</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+    @include('layouts._header')
 
     <div class="container">
-      @yield('content')
-      @include('layouts._footer')
-    </div>    
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
   </body>
 </html>
