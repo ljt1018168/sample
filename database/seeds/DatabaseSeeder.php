64cbf899b23b23aa6537018219cc1e7acea7dda7
liujuntao <ljt1018168@163.com>
2017-12-29 06:18:20 +0000

    finish user edit ,update,index,and destory actions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->call(UsersTableSeeder::class);
+
+        Model::reguard();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::get('login','SessionsController@create')->name('login');
 Route::post('login','SessionsController@store')->name('login');
 Route::delete('logout','SessionsController@destory')->name('logout');
 
+Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
+
+
 
