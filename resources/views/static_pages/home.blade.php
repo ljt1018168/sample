3d30312b2f0eccbeaded2cb932ea048fb2ac506d
liujuntao <ljt1018168@163.com>
2018-01-03 07:39:11 +0000

    add user following

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,10 +7,15 @@
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h3>微博列表</h3>
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+        <section class="stats">
+          @include('shared._stats', ['user' => Auth::user()])
         </section>
       </aside>
     </div>
@@ -26,6 +31,6 @@
       <p>
         <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
       </p>
-    </div>
+    </div>    
   @endif
 @stop
